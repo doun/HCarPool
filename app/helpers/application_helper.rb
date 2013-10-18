4cb0bc38c51da414d781e473ea3d4deac3c1ef89
Barath <barath0811@gmail.com>
2013-10-18 20:23:01 +0530

    Adding areas to Search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	require 'rexml/document'
+
+	def getAreas()
+		file = File.new("app/assets/xml/Properties.xml")
+		doc = REXML::Document.new file
+		areas = []
+		doc.elements.each('//Area/option') do |ele|
+			areas << [ele.text, ele.text]  #Now the options are text-value pairs.
+		end
+
+		areas
+	end
 end
