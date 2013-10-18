4abc0b892fd5a200fb9e50ca50019e821dac5c85
Suresh Sambasivam <suresh.sambasivam@gmail.com>
2013-10-18 20:01:44 +0530

    Modified home page and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Hackathon13Carpool::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
-  resources :homes
-
-resources :routes
+  resources :routes
 
   resources :resource_schedules
 
