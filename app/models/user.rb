f723a633b1c9e1c90ae31975be924f93379348a2
Suresh Sambasivam <suresh.sambasivam@gmail.com>
2013-10-19 02:21:31 +0530

    merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password,:remember_me
   attr_accessible :provider, :uid, :name, :role_ids, :mobile, :image_url
-
+  attr_accessible :preference_attributes
   has_one :preference
-  has_many :user_resourece 
+  has_many :user_resource 
   has_many :venues
   has_many :reviews
   has_and_belongs_to_many :roles
-  
+  accepts_nested_attributes_for :preference, :user_resource
+
   validates :name, :presence => true
 
  	def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
--- app/models/user_resource.rb
+++ app/models/user_resource.rb
@@ -1,4 +1,20 @@
 class UserResource < ActiveRecord::Base
   belongs_to :user
   attr_accessible :acNonac, :carModel, :carNumber, :driverName, :imageUrl, :isOwned, :isSelfDrive, :maxCapacity
+
+  validates :acNonac, presence: true
+  validates :carModel, presence: true
+  validates :carNumber, presence: true
+  validates :isOwned, presence: true
+  validates :isSelfDrive, presence: true
+  validates :maxCapacity, presence: true
+  
+  validates_inclusion_of :carModel, :in => ['Indiga', 'Innova', 'Any']
+
+  def carModel
+    read_attribute(:carModel)
+  end
+  def carModel= (value)
+    write_attribute(:carModel, value)
+  end
 end
