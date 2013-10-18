062aa73975590fa1b0db4775f0e4699b341460c7
Suresh Sambasivam <suresh.sambasivam@gmail.com>
2013-10-18 12:20:42 +0530

    Initial Checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackathon13Carpool::Application.config.secret_token = '7ca4acf3a5b82dd462c93a8a4dc2b6893a0b868f27ba57d70c9f903a5d2f1583a2c4fd7a8ad2789e289225e1c27c9aca2c46482e46947345b97821450fc300ce'
