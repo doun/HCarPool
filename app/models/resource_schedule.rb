11322b00bdc8201c9edf03477695dfa19abca882
Suresh Sambasivam <suresh.sambasivam@gmail.com>
2013-10-19 12:01:26 +0530

    Modify resource_schedule model

--- app/models/resource_schedule.rb
+++ app/models/resource_schedule.rb
@@ -1,6 +1,8 @@
 class ResourceSchedule < ActiveRecord::Base
   attr_accessible :available, :destination, :isconfirmed, :ishireconfirmed, :isowner, 
   :isreadytohire, :routeno, :startplace, :starttime, :userId, :userResourceId, :acNonac
+  belongs_to :user, :foreign_key => "userId"
+  accepts_nested_attributes_for :user
 
 
   def self.search start, dest, ispas
@@ -10,6 +12,7 @@ class ResourceSchedule < ActiveRecord::Base
   		results = ResourceSchedule.where(startplace: start ).where(destination: dest).where(isowner: true)
   	end
   	#results = ResourceSchedule.all
+    results.joins(:user).includes(:user)
   end
 
 end
